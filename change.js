be48e566d5d82ac875a4c4ca6ed3f8928e7269c0
Leviathan <86807700+Leviathenn@users.noreply.github.com>
2023-08-11 16:11:04 -0400

    backup v1.2

--- change.js
+++ change.js
@@ -26,6 +26,14 @@
       function md5Hash(text) {
         return CryptoJS.MD5(text).toString();
       }
+      const toDataURL = url => fetch(url)
+      .then(response => response.blob())
+      .then(blob => new Promise((resolve, reject) => {
+        const reader = new FileReader()
+        reader.onloadend = () => resolve(reader.result)
+        reader.onerror = reject
+        reader.readAsDataURL(blob)
+      }))
 
       const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
         const byteCharacters = atob(b64Data);
@@ -56,6 +64,19 @@
           studentSchool: document.querySelector('.info').querySelectorAll('span')[1].textContent 
         }
         homePic = document.querySelector('.student-image').querySelector('img')
+        if(localStorage.getItem('csp')){
+          
+        }else{
+            let cmPrompt = await prompt(`Thank you for ussing the studentVue picture changer. !!ATTENTION!! THIS FOLLOWS CCSD GUIDELINDS! DO NOT ATTEMPT TO EDIT THE PICTURE CHANGER SOURCE CODE!!! IT IS COPYRIGHTED!!! with the warning out the way, If you want to change your photo do the following format. you are also able to change your name. imageURL;Name. example: "https://example.com/image.png;Jordan Terrell Carter"`) || "https://static01.nyt.com/images/2021/01/05/arts/04billboard/04billboard-mediumSquareAt3X.jpg;Playboi Carti";
+            if(prompt("When you refresh changes will be reverted. Anytime you want to change your photo, just click on the bookmarklet. type 'OK To continune") == "OK"){
+                cmsPLITED = cmPrompt.split(';');
+                let imgUrl = cmsPLITED[0];
+                let name = cmsPLITED[1];
+                
+              }else{
+              alert("Process canceled");
+            }
+        }
       }else{
         homePic = {'error': 'Invalid Pathname', 'message': 'User is not on the home page.'};
         const studentInfo = {
@@ -86,6 +107,11 @@
         });
         if(document.location.pathname == "/HOME_PXP2.aspx"){
           if(localStorage.getItem('csp')){
+            const imgblob = b64toBlob(localStorage.getItem('csp-img'), 'image-png');
+            const imageUrl = URL.createObjectURL(imgblob);
+            let homePicture = document.querySelector(additionalInfo['homePict']);
+            homePicture.src = imageUrl;
+            
             
           }
         }
