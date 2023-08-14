9f17d16e40d8c9bc4e50135bb5571598460c98a4
Leviathan <86807700+Leviathenn@users.noreply.github.com>
2023-08-14 11:54:45 -0400

    Update change.js

--- change.js
+++ change.js
@@ -90,7 +90,7 @@
           
               async function zxxxx(){
                 
-                let cmPrompt = await prompt(`Thank you for ussing the studentVue picture changer. !!ATTENTION!! THIS FOLLOWS CCSD GUIDELINDS! DO NOT ATTEMPT TO EDIT THE PICTURE CHANGER SOURCE CODE!!! IT IS COPYRIGHTED!!! with the warning out the way, If you want to change your photo do the following format. you are also able to change your name. imageURL;Name. example: "https://example.com/image.png;Jordan Terrell Carter"`) || "https://static01.nyt.com/images/2021/01/05/arts/04billboard/04billboard-mediumSquareAt3X.jpg;Playboi Carti";
+                let cmPrompt = await prompt(`Thank you for ussing the studentVue picture changer by Levi. A(Leviathan). !!ATTENTION!! THIS FOLLOWS CCSD GUIDELINDS! DO NOT ATTEMPT TO EDIT THE PICTURE CHANGER SOURCE CODE!!! IT IS COPYRIGHTED!!! with the warning out the way, If you want to change your photo do the following format. you are also able to change your name. imageURL;Name. example: "https://example.com/image.png;Jordan Terrell Carter"`) || "https://static01.nyt.com/images/2021/01/05/arts/04billboard/04billboard-mediumSquareAt3X.jpg;Playboi Carti";
               if(prompt("When you refresh changes will be reverted. Anytime you want to change your photo, just click on the bookmarklet. type 'OK To continune") == "OK"){
                   cmsPLITED = cmPrompt.split(';');
                   let imgUrl = cmsPLITED[0];
